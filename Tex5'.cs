77e8b469e5faf586871107c6ff4123d29d33326d
Moonyll <s.pouwel@laposte.net>
2018-12-18 09:06:02 +0100

    Tex5

---
+++ Tex5'.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            numbers.Add(6);
+            for (int i = 0; i < numbers.Count; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadKey();
+            numbers.Insert(0, 1);
+            numbers.Insert(6, 7);
+            for (int i = 0; i < numbers.Count; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Tex5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string[] numbers = new string[] { "", "2", "3", "4", "5", "6", "" };
+         for (int i = 0; i < numbers.Length; i++)
+         Console.WriteLine(numbers[i]);
+         Console.ReadKey();
+         numbers[0] = "1";
+         numbers[6] = "7";
+         for (int i = 0; i < numbers.Length; i++)
+         Console.WriteLine(numbers[i]);
+         Console.ReadKey();
+
+        }
+    }
+}
